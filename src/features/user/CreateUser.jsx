3643c15dda779d7c08c7dba328bbe82ad90eb32a
Tauseef Mushtaq <tauseef205005@gmail.com>
2025-09-15 18:51:45 +0500

    changes made

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { updateName } from "./userSlice";
-import { redirect, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -13,7 +13,7 @@ function CreateUser() {
     e.preventDefault();
     if (!username) return;
     dispatch(updateName(username));
-    navigate("/menu");
+    navigate('/menu');
   }
 
   return (
@@ -30,7 +30,7 @@ function CreateUser() {
         className="input mb-8 w-72"
       />
 
-      {username !== "" && (
+      {username !== '' && (
         <div>
           <Button type="primary">Start ordering</Button>
         </div>
